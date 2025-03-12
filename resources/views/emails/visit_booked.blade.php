ec7dffb5b4819fd43f9860488989cc67842ea4ca
TheDonDev <donaldmwanga33@gmail.com>
2025-03-12 05:49:56 +0300

    Refactor visit booked email template to use HTML structure for improved readability and presentation; update routes to include View facade.

--- resources/views/emails/visit_booked.blade.php
+++ resources/views/emails/visit_booked.blade.php
@@ -1,22 +1,23 @@
-@component('mail::message')
-# Your Visit has been Booked!
-
-Dear {{ $visitor_name }},
-
-Your visit details are as follows:
-
-**Visit Details:**
-- Visit Number: {{ $visit_number }}
-- Host Name: {{ $host_name }}
-- Host Email: {{ $host_email }}
-- Host Phone Number: {{ $host_number }}
-- Visit Date: {{ $visit_date }}
-- Visit Time: {{ $visit_time }}
-- Visit Type: {{ $visit_type }}
-- Visit Facility: {{ $visit_facility }}
-
-Thank you for booking your visit!
-
-Best regards,
-The VisiTrack Team
-@endcomponent
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Visit has been Booked!</title>
+</head>
+<body>
+    <h1>Your Visit has been Booked!</h1>
+    <p>Dear {{ $visitor_name }},</p>
+    <p>Your visit details are as follows:</p>
+    <ul>
+        <li><strong>Visit Number:</strong> {{ $visit_number }}</li>
+        <li><strong>Host Name:</strong> {{ $host_name }}</li>
+        <li><strong>Host Email:</strong> {{ $host_email }}</li>
+        <li><strong>Host Phone Number:</strong> {{ $host_number }}</li>
+        <li><strong>Visit Date:</strong> {{ $visit_date }}</li>
+        <li><strong>Visit Time:</strong> {{ $visit_time }}</li>
+        <li><strong>Visit Type:</strong> {{ $visit_type }}</li>
+        <li><strong>Visit Facility:</strong> {{ $visit_facility }}</li>
+    </ul>
+    <p>Thank you for booking your visit!</p>
+    <p>Best regards,<br>The VisiTrack Team</p>
+</body>
+</html>
